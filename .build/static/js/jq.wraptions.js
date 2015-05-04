d909c715ccca78d2717da30e6402d0e1e7daff72
scottrcollins <scottrileycollins@gmail.com>
2015-05-04 16:40:49 -0500

    Added More tags
    Second content section SHOULD read ‘Trending in Chicago’

--- .build/static/js/jq.wraptions.js
+++
@@ -1,8 +0,0 @@
-(function($){
-
-$.fn.wraption = function(){
-	var caption = this.attr('title');
-	this.wrap('<div class="wraption-wrap"></div>').after('<span class="wraption-caption">' + caption + '</span>');
-};
-
-}(jQuery));
